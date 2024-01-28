8c420eccb8a46db31ef46cdff5ca0598ca7bf8d2
Mehmet <mehmetext68@gmail.com>
2024-01-28 09:57:43 +0300

    add dotenv config and port environment

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app = express();
+const port = process.env.PORT || 5002;
 
-app.get("/", (req: Request, res: Response) => {
-  const data = { foo: "bar" };
-  console.log(data);
-  res.json(data);
+app.get("/", (req, res) => {
+  res.json({ mesaj: "ana sayfa" });
 });
 
-app.listen(4000, () => {
-  console.log("Server çalışıyor...");
+app.listen(port, () => {
+  console.log(`Server ${port} portunda çalışıyor...`);
 });
