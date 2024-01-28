ba0033b49d8e6983501e74474ba0190c721fa54a
Mehmet <mehmetext68@gmail.com>
2024-01-28 10:58:19 +0300

    add register function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import user from "../models/user.model";
+import bcrypt from "bcrypt";
 
 export default class AuthController {
   static async login(req: Request, res: Response) {
@@ -7,7 +9,23 @@ export default class AuthController {
     res.json({ neler: "oldu" });
   }
 
-  static async register(req: any, res: any) {
-    console.log(req.body);
+  static async register(req: Request, res: Response) {
+    const userCheck = await user.findOne({ email: req.body.email });
+
+    if (userCheck) {
+      console.log("Girilen email kullanımda!");
+      return res.send({ status: false });
+    }
+
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+
+    const creatingUser = new user(req.body);
+    const response = await creatingUser.save();
+
+    res.json({
+      status: true,
+      data: response,
+      message: "Kayıt başarıyla eklendi.",
+    });
   }
 }
