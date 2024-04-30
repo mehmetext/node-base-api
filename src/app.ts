54ce1c7e9e7fc7b2d7bbf4900e0e72d9951b908c
Mehmet <mehmetext68@gmail.com>
2024-04-30 17:07:35 +0300

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("asd");
+import e from "express";
+
+const app = e();
+
+app.get("/", (req, res) => {
+  res.json({ foo: "bar" });
+});
+
+app.listen(4000, () => {
+  console.log("Server is running...");
+});
