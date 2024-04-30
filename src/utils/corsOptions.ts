3d60ff5c260746f050fc3e694780ea7a5d2e3f76
Mehmet <mehmetext68@gmail.com>
2024-04-30 17:03:48 +0300

    delete old files and add typescript

--- src/utils/corsOptions.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-
-const whiteList = ["http://localhost:3000"];
-
-export default function corsOptions(req: Request, callback: any) {
-  const options = { origin: whiteList.includes(req.headers.origin ?? "") };
-
-  callback(null, options);
-}
