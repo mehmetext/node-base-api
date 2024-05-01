984c67ca15a3d9b6a09b8c81953a9aa9c629cce7
Mehmet <mehmetext68@gmail.com>
2024-05-02 00:50:26 +0300

    add cors options

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import e from "express";
 import dotenv from "dotenv";
 import router from "./routers";
 import errorHandler from "./middlewares/error-handler";
+import cors from "cors";
+import corsOptions from "./utils/cors-options";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
@@ -12,6 +14,7 @@ const app = e();
 //Middlewares
 app.use(e.json());
 app.use(e.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.use("/api/v1", router);
 app.use(errorHandler);
