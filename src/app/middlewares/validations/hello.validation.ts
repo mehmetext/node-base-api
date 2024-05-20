cdfb44f60036b29b9526cacd9f6eb6fba9c08f72
Mehmet <mehmetext68@gmail.com>
2024-05-20 21:22:02 +0300

    Update hello.validation.ts

--- src/app/middlewares/validations/hello.validation.ts
+++ src/app/middlewares/validations/hello.validation.ts
@@ -3,12 +3,14 @@ import RootValidation from "../../../lib/root-validation";
 import Middleware from "../../../lib/types/middleware";
 
 export default class HelloValidation extends RootValidation {
-  static post: Middleware = async (req, res, next) => {
-    const schema = Joi.object({
-      name: Joi.string().required(),
-      surname: Joi.string().required(),
-    });
-
-    await super.validate(req, res, next, schema);
-  };
+  static post: Middleware = async (req, res, next) =>
+    await super.validate(
+      req,
+      res,
+      next,
+      Joi.object({
+        name: Joi.string().required(),
+        surname: Joi.string().required(),
+      })
+    );
 }
